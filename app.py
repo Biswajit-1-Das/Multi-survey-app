2066265f1fdd64303cc7b3495a1347d9df79c7fe
Biswajit Das <biswajitdas90511@gmail.com>
2025-08-06 11:55:10 +0530

    Fix: Bind Flask app to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -37,4 +37,5 @@ def submit_form(survey_type):
     return render_template("thank_you.html", survey=survey_type.upper())
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True,host='0.0.0.0', port=port)
